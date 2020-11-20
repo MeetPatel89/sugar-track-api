9714c54ad1ecbda3b90dadd173ed1cc906ed95f0
Meet Patel <meetaiims@yahoo.com>
2020-11-20 11:52:48 -0600

    insert dummy data into users

---
+++ seeds/insert_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users
+    (fullname, username, password)
+VALUES
+    ('Nautilus Shell', 'nautilus89', 'RandomNautilus12#'),
+    ('Fibonacci Spiral', 'fibonacci89', 'RandomFibonacci12#'),
+    ('Logarithmic Spiral', 'logarithmic89', 'RandomLogarithmic12#');
+
+
