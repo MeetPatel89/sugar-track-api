b77e4dcd82704f53a37aba32130447b63fb6effc
Meet Patel <meetaiims@yahoo.com>
2020-11-20 12:45:35 -0600

    insert dummy data into meds_logs

---
+++ seeds/insert_meds_logs.sql
@@ -0,0 +1,9 @@
+INSERT INTO meds_logs
+    (meds, time, user_id, date)
+VALUES
+    ('metformin', '10:00', 1, '2020-11-17'),
+    ('insulin', '04:00', 1, '2020-11-17'),
+    ('herbal', '09:00', 1, '2020-11-17'),
+    ('metformin', '10:00', 1, '2020-11-18'),
+    ('insulin', '04:00', 1, '2020-11-18'),
+    ('herbal', '09:00', 1, '2020-11-18')
