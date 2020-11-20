e62aa1ee77ede08a6853e24231122866a2c4853a
Meet Patel <meetaiims@yahoo.com>
2020-11-20 12:53:17 -0600

    insert dummy data into meals_logs

---
+++ seeds/insert_meals_logs.sql
@@ -0,0 +1,9 @@
+INSERT INTO meals_logs
+    (meals, time, user_id, date)
+VALUES 
+    ('Egg Sandwich', '9:00', 1, '2020-11-17'),
+    ('Rice', '12:30', 1, '2020-11-17'),
+    ('Heavy Dinner', '08:30', 1, '2020-11-17'),
+    ('Chocolate Sandwich', '9:00', 1, '2020-11-18'),
+    ('Chipotle', '12:30', 1, '2020-11-18'),
+    ('Super Heavy Dinner', '08:30', 1, '2020-11-18');
