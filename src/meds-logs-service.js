66c60eefae85bf7bd302f5e3ba1a3f643c20f85f
Meet Patel <meetaiims@yahoo.com>
2020-12-08 23:19:31 -0600

    refactor meds-logs-service-object

--- src/meds-logs-service.js
+++ src/meds-logs-service.js
@@ -1,62 +1,41 @@
 const MedsLogsService = {
-    insertMedLog(knex, newMed) {
-        return knex
-            .insert(newMed)
-            .into('meds_logs')
-            .returning('*')
-    },
-    getMedsLogs(knex) {
-        return knex
-            .select('*')
-            .from('meds_logs')
-    },
-    getMedsByDateTime(knex, dateTime) {
-        return knex
-                .select('*')
-                .from('meds_logs')
-                .where({
-                    dateTime
-                })
-    },
-    sortMedsLogsByDateTime(knex, user_id, sort) {
-        return knex
-                .from('meds_logs')
-                .where({
-                    user_id
-                })
-                .orderBy(sort)
-    },
-    getMedsLogsByUserId(knex, user_id) {
-        return knex
-                .select('*')
-                .from('meds_logs')
-                .where({
-                    user_id
-                })
-    },
-    deleteMedsLogsById(knex, id) {
-        return knex
-                .from('meds_logs')
-                .where({id})
-                .delete()
-                .returning('*')
-    },
-    
-    updateMedsLog(knex, id, newMedsLog) {
-        return knex('meds_logs')
-                .where({id})
-                .update(newMedsLog)
-                .returning('*')
-    },
-    getMedsLogById(knex, user_id, id) {
-        return knex 
-                .select('*')
-                .from('meds_logs')
-                .where({
-                    user_id,
-                    id
-                })
-    }
-}
+  insertMedLog(knex, newMed) {
+    return knex.insert(newMed).into('meds_logs').returning('*');
+  },
+  getMedsLogs(knex) {
+    return knex.select('*').from('meds_logs');
+  },
+  getMedsByDateTime(knex, dateTime) {
+    return knex.select('*').from('meds_logs').where({
+      dateTime,
+    });
+  },
+  sortMedsLogsByDateTime(knex, user_id, sort) {
+    return knex
+      .from('meds_logs')
+      .where({
+        user_id,
+      })
+      .orderBy(sort);
+  },
+  getMedsLogsByUserId(knex, user_id) {
+    return knex.select('*').from('meds_logs').where({
+      user_id,
+    });
+  },
+  deleteMedsLogsById(knex, id) {
+    return knex.from('meds_logs').where({ id }).delete().returning('*');
+  },
+
+  updateMedsLog(knex, id, newMedsLog) {
+    return knex('meds_logs').where({ id }).update(newMedsLog).returning('*');
+  },
+  getMedsLogById(knex, user_id, id) {
+    return knex.select('*').from('meds_logs').where({
+      user_id,
+      id,
+    });
+  },
+};
 
 module.exports = MedsLogsService;
