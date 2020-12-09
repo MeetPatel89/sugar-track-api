aeddf25355b6195aff78eec5e5a7df24582187ef
Meet Patel <meetaiims@yahoo.com>
2020-12-08 23:17:37 -0600

    refactor glucose-logs-service object

--- src/glucose-logs-service.js
+++ src/glucose-logs-service.js
@@ -1,75 +1,48 @@
 const GlucoseLogsService = {
-    insertGlucoseLog(knex, newGlucoseLog) {
-        return knex
-                .insert(newGlucoseLog)
-                .into('glucose_logs')
-                .returning('*')
-    },
-    getGlucoseLogs(knex) {
-        return knex
-                .select('*')
-                .from('glucose_logs')
-    },
-    getGlucoseLogsByDateTime(knex, dateTime) {
-        return knex 
-                .select('*')
-                .from('glucose_logs')
-                .where({
-                    dateTime
-                })
-    },
-    deleteGlucoseLogById(knex, id) {
-        return knex
-                .from('glucose_logs')
-                .where({id})
-                .delete()
-                .returning('*')
-    },
-    getGlucoseLogsByUserId(knex, user_id) {
-        return knex
-                .select('*')
-                .from('glucose_logs')
-                .where({
-                    user_id
-                })
-    },
-    sortGlucoseLogs(knex, sort) {
-        return knex
-                .from('glucose_logs')
-                .orderBy(sort)
-    },
-    sortGlucoseLogsByDateTime(knex, user_id, sort) {
-        return knex
-                .from('glucose_logs')
-                .where({
-                    user_id
-                })
-                .orderBy(sort)
-    },
-    sortAllLogsByDateTime(knex, user_id, sort) {
-        return knex
-                .from('glucose_logs', 'meals_logs', 'meds_logs')
-                .where({
-                    user_id
-                })
-                .orderBy(sort)
-    },
-    updateGlucoseLog(knex, id, newGlucoseLog) {
-        return knex('glucose_logs')
-                .where({id})
-                .update(newGlucoseLog)
-                .returning('*')
-    },
-    getGlucoseLogsById(knex, id) {
-        return knex
-                .select('*')
-                .from('glucose_logs')
-                .where({
-                    
-                    id
-                })
-    }
-}
+  insertGlucoseLog(knex, newGlucoseLog) {
+    return knex.insert(newGlucoseLog).into('glucose_logs').returning('*');
+  },
+  getGlucoseLogs(knex) {
+    return knex.select('*').from('glucose_logs');
+  },
+  deleteGlucoseLogById(knex, id) {
+    return knex.from('glucose_logs').where({ id }).delete().returning('*');
+  },
+  getGlucoseLogsByUserId(knex, user_id) {
+    return knex.select('*').from('glucose_logs').where({
+      user_id,
+    });
+  },
+  sortGlucoseLogs(knex, sort) {
+    return knex.from('glucose_logs').orderBy(sort);
+  },
+  sortGlucoseLogsByDateTime(knex, user_id, sort) {
+    return knex
+      .from('glucose_logs')
+      .where({
+        user_id,
+      })
+      .orderBy(sort);
+  },
+  sortAllLogsByDateTime(knex, user_id, sort) {
+    return knex
+      .from('glucose_logs', 'meals_logs', 'meds_logs')
+      .where({
+        user_id,
+      })
+      .orderBy(sort);
+  },
+  updateGlucoseLog(knex, id, newGlucoseLog) {
+    return knex('glucose_logs')
+      .where({ id })
+      .update(newGlucoseLog)
+      .returning('*');
+  },
+  getGlucoseLogsById(knex, id) {
+    return knex.select('*').from('glucose_logs').where({
+      id,
+    });
+  },
+};
 
 module.exports = GlucoseLogsService;
-
