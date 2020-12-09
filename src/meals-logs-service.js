11cca539202187ee9870e54dfeaa3a8c3721b62c
Meet Patel <meetaiims@yahoo.com>
2020-12-08 23:18:37 -0600

    refactor meals-logs-service object

--- src/meals-logs-service.js
+++ src/meals-logs-service.js
@@ -1,55 +1,35 @@
 const MealsLogsService = {
-    getMealsLogs(knex) {
-        return knex
-                .select('*')
-                .from('meals_logs')
-    },
-    insertMealsLog(knex, newMealsLog) {
-        return knex
-                .insert(newMealsLog)
-                .into('meals_logs')
-                .returning('*')
-    },
-    sortMealsLogsByDateTime(knex, user_id, sort) {
-        return knex
-                .from('meals_logs')
-                .where({
-                    user_id
-                })
-                .orderBy(sort)
-    },
-    getMealsLogsByUserId(knex, user_id) {
-        return knex
-                .from('meals_logs')
-                .select('*')
-                .where({
-                    user_id
-                })
-    },
-    deleteMealsLogsById(knex, id) {
-        return knex
-                .from('meals_logs')
-                .where({id})
-                .delete()
-                .returning('*')
-    },
-    
-    updateMealsLog(knex, id, newMealLog) {
-        return knex('meals_logs')
-                .where({id})
-                .update(newMealLog)
-                .returning('*')
-    },
-    
-    getMealsLogById(knex, user_id, id) {
-        return knex 
-                .select('*')
-                .from('meals_logs')
-                .where({
-                    user_id,
-                    id
-                })
-    }
-}
+  getMealsLogs(knex) {
+    return knex.select('*').from('meals_logs');
+  },
+  insertMealsLog(knex, newMealsLog) {
+    return knex.insert(newMealsLog).into('meals_logs').returning('*');
+  },
+  sortMealsLogsByDateTime(knex, user_id, sort) {
+    return knex
+      .from('meals_logs')
+      .where({
+        user_id,
+      })
+      .orderBy(sort);
+  },
+  getMealsLogsByUserId(knex, user_id) {
+    return knex.from('meals_logs').select('*').where({
+      user_id,
+    });
+  },
+  deleteMealsLogsById(knex, id) {
+    return knex.from('meals_logs').where({ id }).delete().returning('*');
+  },
+  updateMealsLog(knex, id, newMealLog) {
+    return knex('meals_logs').where({ id }).update(newMealLog).returning('*');
+  },
+  getMealsLogById(knex, user_id, id) {
+    return knex.select('*').from('meals_logs').where({
+      user_id,
+      id,
+    });
+  },
+};
 
 module.exports = MealsLogsService;
